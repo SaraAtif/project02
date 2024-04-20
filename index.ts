1c0a9763f2e0daa6b6673d4161120d20f63f2168
Sara <saraatif135@gmail.com>
2024-04-21 01:23:01 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+//Question #04 
+console.log(`Albert Einstien once said,"A person who never made a mistake never tried anything new"`);
+
+//Question #05
+let famous_person = "Albert Einstien";
+//let message = `"A person who never made a mistake never tried anything new"`
+//console.log(famous_person + " once said " + message);
+let message = `${famous_person} once said, "A person who never made a mistake never tried anything new"`
+console.log(message);  
+
+//Question #06
+let personName = "\t\n Sara \t\n"
+console.log(personName);//show the name with whitespace
+console.log(personName.trim());//show the name without whitespace
